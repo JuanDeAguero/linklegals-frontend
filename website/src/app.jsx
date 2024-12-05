3bddfeed4ae0ba0f14982ef16d51d8ec3b2e79ad
juandeaguero <juanmgdeaguero@gmail.com>
2024-12-05 00:58:29 -0500

    services + contact

--- website/src/app.jsx
+++ website/src/app.jsx
@@ -71,7 +71,7 @@ const About = ({ isMobile, showMenu, scrollY }) => {
         }
     }
 
-    const sections = [-1, 150 - 40, 500 - 40, 950 - 40, 1110 - 40, 1365 - 40, 1560 - 40, 1730 - 40, 1910 - 40, 2510 - 40, 10000]
+    const sections = [-1, 130, 840, 1640, 1810, 2060, 2250, 2410, 2600, 3610, 10000]
 
     const determineGuideStyle = (number, scrollY) => {
         if (number === 0) return determineGuideStyleHelper(scrollY, sections[0], sections[1])
@@ -124,12 +124,14 @@ const About = ({ isMobile, showMenu, scrollY }) => {
         <div className="about-text about-quote">“The future of human work is in specific domain expertise. Understanding the challenges and then using autonomously intelligent technology to help us provide solutions.”</div>
         <div className="about-text about-jensen">- Jensen Huang</div>
         <div className="about-text">Our journey starts with recognizing the vital role of domain experts in guiding AI to solve real-world problems. In many sectors, experts see the potential of existing technologies if adapted correctly. Instead of waiting for these changes to happen, we took action to create practical AI-driven tools. We saw an opportunity to make AI beneficial for those with fewer resources by helping them share their stories more effectively.</div>
+        <img className="about-img" src="img1.jpg" />
         <div className="about-subtitle" ref={identifyingProblemRef}>Identifying the Problem</div>
         <div className="about-text">Many AI innovations are not addressing the needs of underrepresented communities. Specifically, we identified a few key problems to address:</div>
-        <div className="about-text">Repetitive Storytelling: Clients expend valuable time and energy recounting their experiences to multiple individuals involved in their case, resulting in fatigue and the omission of important details.</div>
-        <div className="about-text">Limited Options: Time constraints force clients to accept less optimal choices.</div>
-        <div className="about-text">Document Complexity: The complex nature of many documents renders them difficult for clients to comprehend.</div>
-        <div className="about-text">Language Barriers: These barriers pose substantial challenges to accessing legal services.</div>
+        <div className="about-text"><span className="about-text-underline">Repetitive Storytelling:</span> Clients expend valuable time and energy recounting their experiences to multiple individuals involved in their case, resulting in fatigue and the omission of important details.</div>
+        <div className="about-text"><span className="about-text-underline">Limited Options:</span> Time constraints force clients to accept less optimal choices.</div>
+        <div className="about-text"><span className="about-text-underline">Document Complexity:</span> The complex nature of many documents renders them difficult for clients to comprehend.</div>
+        <div className="about-text"><span className="about-text-underline">Language Barriers:</span> These barriers pose substantial challenges to accessing legal services.</div>
+        <img className="about-img" src="img2.jpg" />
         <div className="about-subtitle" ref={missionVisionRef}>Mission and Vision</div>
         <div className="about-text">Our mission is to innovate, connect, and empower by creating AI solutions that are advanced, accessible, and socially responsible. We envision a platform that prioritizes user safety and confidentiality.</div>
         <div className="about-subtitle" ref={initialStepsRef}>Initial Steps and Development</div>
@@ -143,21 +145,164 @@ const About = ({ isMobile, showMenu, scrollY }) => {
         <div className="about-title" ref={researchRef}>Research</div>  
         <div className="about-text">We are committed to research and innovation to better serve our community and service providers. We're developing a mobile app that uses AI to create practical solutions, prioritizing user safety, confidentiality, and ethical use of emerging technologies.</div>
         <div className="about-text">Our key initiatives include:</div>
-        <div className="about-text">User-Centric Design: Ensuring our app is intuitive and accessible to all users, regardless of their tech proficiency.</div>
-        <div className="about-text">Continuous Improvement: Regularly updating our technology and practices based on user feedback and advancements in the field.</div>
-        <div className="about-text">Community Engagement: Actively involving our community in the development process to ensure the app meets real-world needs.</div>
-        <div className="about-text">Educational Resources: Providing users with resources and support to help them make the most of our technology.</div>
+        <div className="about-text"><span className="about-text-underline">User-Centric Design:</span> Ensuring our app is intuitive and accessible to all users, regardless of their tech proficiency.</div>
+        <div className="about-text"><span className="about-text-underline">Continuous Improvement:</span> Regularly updating our technology and practices based on user feedback and advancements in the field.</div>
+        <div className="about-text"><span className="about-text-underline">Community Engagement:</span> Actively involving our community in the development process to ensure the app meets real-world needs.</div>
+        <div className="about-text"><span className="about-text-underline">Educational Resources:</span> Providing users with resources and support to help them make the most of our technology.</div>
         <div className="about-text">We believe that by integrating these elements, we can create a more inclusive, effective, and secure platform for all.  More detailed information about the application will be available soon.</div>
+        <img className="about-img" src="img3.jpg" />
         <div className="about-title" ref={collaborationsRef}>Collaborations</div>
         <div className="about-text">We proudly collaborate with American University, which supports our research efforts and contributes to the development of our mobile app by providing access to their extensive resources, including faculty expertise, and technical support. Additionally, we are working with non-profits and private attorneys who offer expert legal guidance, help expand our translation and interpretation services, and provide valuable app usability feedback, which we use to better understand their communities and address their needs.</div>
         <div className="about-text">In these early stages, we plan to collaborate with more organizations and professionals who share our vision of making services more accessible and understandable for everyone through the use of emerging technology. Understanding the importance of privacy, safety, and user empowerment, we are dedicated to ensuring these core values are at the forefront of our initiatives.</div>
-        <div className="about-text about-last">Interested in partnering or collaborating with us? Reach out at collaborations@linklegals.com.</div>
+        <div className="about-text">Interested in partnering or collaborating with us? Reach out at <span className="about-text-underline">collaborations@linklegals.com</span>.</div>
+        <img className="about-img-au about-last" src="au.png" />
       </div>
     )
 }
 
-const Services = () => <div className="page"></div>
-const Contact = () => <div className="page"></div>
+const Services = ({ isMobile, showMenu }) => {
+    return(
+    <div className="page" style={{ overflow: showMenu && isMobile ? "hidden" : "auto", height: showMenu && isMobile ? "calc(100vh - 60px)" : "auto" }}>
+        <div className="about-title about-first">Services</div>
+        <div className="about-text">As we develop our mobile application, our project begins with two services, translations for USCIS-related documents and interpretation services for USCIS interviews in select field offices. Explore our services below to learn more about how we can assist you.</div>
+        <div className="about-subtitle">Translations</div>
+        <div className="about-subtitle-2">Accurate and Timely Document Translation Services</div>
+        <img className="about-img" src="img4.jpg" />
+        <div className="about-text">Translations to help you navigate your document needs. Our expert translators ensure precision and cultural sensitivity. Our services include:</div>
+        <div className="about-text"><span className="about-text-underline">Certified Translations:</span> All documents are translated and certified by professionals.</div>
+        <div className="about-text"><span className="about-text-underline">Quick Turnaround:</span> We prioritize timely delivery without compromising quality.</div>
+        <div className="about-text"><span className="about-text-underline">Multilingual Support:</span> Translations available in multiple languages, tailored to your specific needs.</div>
+        <div className="about-text">For a quote, click here or email us at <span className="about-text-underline">translations@LinkLegals.com</span>. We're ready to assist you with all your translation needs!</div>
+        <div className="about-subtitle">Interpretations</div>
+        <div className="about-subtitle-2">Interpretation Services for USCIS interviews</div>
+        <img className="about-img" src="img5.jpg" />
+        <div className="about-text">At LinkLegals, we understand how important clear communication is during your USCIS interviews. Currently, we offer interpretation services for USCIS interviews in the Baltimore and Fairfax Field Offices, with plans to expand our reach. To help you feel confident and prepared, our services include:</div>
+        <div className="about-text"><span className="about-text-underline">Pre-Interview Practice:</span> Sessions to cover what to expect, personal questions, and civic exam practice, ensuring readiness for every aspect of the interview process.</div>
+        <div className="about-text about-last">For more information, email us at <span className="about-text-underline">interpretation@LinkLegals.com</span>. We're here to support you every step of the way!</div>
+    </div>
+    )
+}
+
+const Contact = ({ isMobile, showMenu }) => {
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        subject: "",
+        message: ""
+    })
+
+    const [errors, setErrors] = useState({})
+    const [isSubmitted, setIsSubmitted] = useState(false)
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setFormData({
+            ...formData,
+            [name]: value
+        })
+    }
+
+    const validate = () => {
+        const newErrors = {}
+        if (!formData.name.trim()) newErrors.name = "Name is required."
+        if (!formData.email.trim()) {
+            newErrors.email = "Email is required."
+        } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+            newErrors.email = "Email is invalid."
+        }
+        if (!formData.subject.trim()) newErrors.subject = "Subject is required."
+        if (!formData.message.trim()) newErrors.message = "Message is required."
+        return newErrors
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        const validationErrors = validate()
+        if (Object.keys(validationErrors).length === 0) {
+            // Handle form submission, e.g., send data to a server or EmailJS
+            console.log("Form Data Submitted:", formData)
+            setIsSubmitted(true)
+            setFormData({
+                name: "",
+                email: "",
+                subject: "",
+                message: ""
+            })
+        } else {
+            setErrors(validationErrors)
+        }
+    }
+
+    return (
+        <div className="page" style={{ overflow: showMenu && isMobile ? "hidden" : "auto", height: showMenu && isMobile ? "calc(100vh - 60px)" : "auto" }}>
+            <div className="about-title about-first">Contact</div>
+            <div className="about-text">We are here to help and look forward to connecting with you! Please find the appropriate contact information below for your specific needs.</div>
+            <div className="about-text"><span className="about-text-bold">General Inquiries:</span> For general questions or information about our services, please email us at: <span className="about-text-underline">info@LinkLegals.com</span></div>
+            <div className="about-text"><span className="about-text-bold">Customer Support:</span> If you need assistance with our services, contact us at: <span className="about-text-underline">support@linklegals.com</span></div>
+            <div className="about-text"><span className="about-text-bold">Partnership and Collaboration:</span> Interested in partnering or collaborating with us? Reach out to our collaboration team at: <span className="about-text-underline">collaborations@linklegals.com</span></div>
+            <div className="about-text"><span className="about-text-bold">Media and Press Inquiries:</span> For media and press inquiries, please contact our media relations team: <span className="about-text-underline">press@linklegals.com</span></div>
+            <div className="about-text">You can also use our contact form on this page. We look forward to connecting with you!</div>
+
+            <form className="contact-form" onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="name">Name</label>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleChange}
+                        className={errors.name ? "input-error" : ""}
+                    />
+                    {errors.name && <span className="error">{errors.name}</span>}
+                </div>
+                <div className="form-group">
+                    <label htmlFor="email">Email</label>
+                    <input
+                        type="email"
+                        id="email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                        className={errors.email ? "input-error" : ""}
+                    />
+                    {errors.email && <span className="error">{errors.email}</span>}
+                </div>
+                <div className="form-group">
+                    <label htmlFor="subject">Subject</label>
+                    <input
+                        type="text"
+                        id="subject"
+                        name="subject"
+                        value={formData.subject}
+                        onChange={handleChange}
+                        className={errors.subject ? "input-error" : ""}
+                    />
+                    {errors.subject && <span className="error">{errors.subject}</span>}
+                </div>
+                <div className="form-group">
+                    <label htmlFor="message">Message</label>
+                    <textarea
+                        id="message"
+                        name="message"
+                        value={formData.message}
+                        onChange={handleChange}
+                        className={errors.message ? "input-error" : ""}
+                    ></textarea>
+                    {errors.message && <span className="error">{errors.message}</span>}
+                </div>
+                <button type="submit" className="submit-button">Submit</button>
+                {isSubmitted && (
+                    <div className="success-message">
+                        <h2>Thank you for contacting us!</h2>
+                        <p>We have received your message and will get back to you shortly.</p>
+                    </div>
+                )}
+            </form>
+        </div>
+    )
+}
+
 const TranslatorGPT = () => <div className="page"></div>
 
 const BackgroundGradient = () => {
@@ -229,8 +374,8 @@ const App = () => {
                     {showMenu ? <div className="nav-right">
                         <Link to="/" onClick={onLinkClick} className="nav-button">Home</Link>
                         <Link to="/about" onClick={onLinkClick} className="nav-button">About</Link>
-                        <Link to="/" onClick={onLinkClick} className="nav-button">Services</Link>
-                        <Link to="/" onClick={onLinkClick} className="nav-button">Contact</Link>
+                        <Link to="/services" onClick={onLinkClick} className="nav-button">Services</Link>
+                        <Link to="/contact" onClick={onLinkClick} className="nav-button">Contact</Link>
                         <Link to="/" onClick={onLinkClick} className="nav-button">Translator GPT</Link>
                         <div className="nav-right-background" />
                     </div> : null}
@@ -242,8 +387,8 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About isMobile={isMobile} showMenu={showMenu} scrollY={scrollY} />} />
-                <Route path="/services" element={<Services />} />
-                <Route path="/contact" element={<Contact />} />
+                <Route path="/services" element={<Services isMobile={isMobile} showMenu={showMenu} />} />
+                <Route path="/contact" element={<Contact isMobile={isMobile} showMenu={showMenu} />} />
                 <Route path="/translator-gpt" element={<TranslatorGPT />} />
             </Routes>
         </div>
